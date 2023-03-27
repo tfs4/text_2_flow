3f671aa211b2709638609463f8cc40f0ddce00ec
thiago <thiago.sousa@ifg.edu.br>
2023-03-27 15:28:16 -0300

    projeto init

--- README.md
+++ README.md
@@ -1,11 +1,5 @@
-# text_to_flowchart
-Proof of concept to show an idea about how to turn text into flowchart
-
-Creator: Lingjing Liu
-
-[![Binder](https://mybinder.org/badge_logo.svg)](https://mybinder.org/v2/gh/LLiu24/text_to_flowchart/HEAD?labpath=demo.ipynb)
 
 
-![image](https://user-images.githubusercontent.com/53161266/207052353-fa7c0337-341a-4898-8013-6f27c049700a.png)
+python -m spacy download pt_core_news_sm
 
-![demo](https://user-images.githubusercontent.com/53161266/198969214-725bbcae-d769-4020-bd27-7de072d2f15b.gif)
+python -m spacy download
--- main.py
+++ main.py
@@ -4,13 +4,8 @@ from additional_viz import viz
 
 if __name__ == '__main__':
     text1 = '''
-    If you choose to steep loose-leaf tea, you'll need a tea infuser.
-
-    Fill the infuser with the amount of tea specified on the package for how many servings you need.
-    Bring fresh, cold water to a rolling boil.
-    Once boiling, pour over the tea infuser (or lower the infuser into the pot) and steep according to the timing above based on your tea variety.
-    Remove the tea infuser from the cup or pot.
-    If desired, finish with milk, your preferred sweetener, or a slice of citrus.
+     Se você optar por infundir chá de folhas soltas, precisará de um infusor de chá. Encha o infusor com a quantidade de chá especificada na embalagem para quantas porções você precisa. Leve água fresca e fria para ferver. Depois de ferver, despeje sobre o infusor de chá (ou abaixe o infusor na panela) e deixe em infusão de acordo com o tempo acima com base na sua variedade de chá. Remova o infusor de chá da xícara ou bule.
+     Se desejar, finalize com leite, adoçante de sua preferência ou uma rodela de frutas cítricas.
     '''
 
     text1_title = 'teste'
